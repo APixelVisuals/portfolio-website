af4c1c4e0c0328b5e1eb46326176bab37c453f4d
APixel Visuals <apixelvisualsofficial@gmail.com>
2019-04-13 15:00:46 -0700

    Added navbar

---
+++ components/NavBar.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "../global.css";
+import "./navBar.css";
+
+export default class NavBar extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this.updateSocialTagY = e => this.setState({ socialTagY: e.pageY - 18 });
+    };
+
+    render = () => {
+        return (
+            <div id="nav-bar">
+
+                <a className="home-button" href="/"><img src="/static/apixel.png" /></a>
+
+                <div className="bar">
+                    <a className="button" href="/">🏠</a>
+                    <a className="button" href="/about">About</a>
+                    <a className="button" href="/work">Work</a>
+                </div>
+
+                <div className="side-bar">
+                    <a className="button" href="/twitter" target="_blank">Twitter</a>
+                    <a className="button" onMouseOver={() => this.setState({ socialTag: true, socialTagText: "APixel Visuals#2820" })} onMouseOut={() => this.setState({ socialTag: false })}>Discord</a>
+                    <a className="button" onMouseOver={() => this.setState({ socialTag: true, socialTagText: "contact@idkyet.com" })} onMouseOut={() => this.setState({ socialTag: false })}>Email</a>
+                </div>
+
+                <div className="social-tag" style={{ opacity: this.state.socialTag && "1", top: `${this.state.socialTagY}px` }}>
+                    <h1>{this.state.socialTagText}</h1>
+                </div>
+
+            </div>
+        );
+    };
+
+    componentDidMount = () => document.addEventListener("mousemove", this.updateSocialTagY);
+    componentWillUnmount = () => document.removeEventListener("mousemove", this.updateSocialTagY);
+
+};
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "../components/Head";
+import NavBar from "../components/NavBar";
 import "../global.css";
 import "./index.css";
 
@@ -13,8 +14,7 @@ export default class Index extends React.Component {
                     title="APixel Visuals"
                     description="A description about me goes here lol"
                 />
-
-                
+                <NavBar />
 
             </div>
         );

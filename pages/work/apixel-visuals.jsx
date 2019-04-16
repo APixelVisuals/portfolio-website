67d6537ff460641ddf1386c443305e9706603a12
APixel Visuals <apixelvisualsofficial@gmail.com>
2019-04-15 18:55:36 -0700

    Added some color to `work/apixel-visuals`

--- pages/work/apixel-visuals.jsx
+++ pages/work/apixel-visuals.jsx
@@ -34,8 +34,8 @@ export default class APixelVisuals extends React.Component {
                         <h1 className="text">{"APixel Visuals".split("").map((l, i) => <span className={this.state.titles.apixelVisuals[i]}>{l}</span>)}</h1>
                     </div>
 
-                    <h1 className="info">I made this website mainly to showcase my work to potential clients (maybe that's you?), as well as anyone else who might be interested in the things I make.</h1>
-                    <h1 className="info">One thing I love about this website is that it's my own. That means that I can add a bunch of Easter eggs around it. Seriously, it's actually kinda fun making them.</h1>
+                    <h1 className="info">I made this website mainly to <span>showcase</span> my work to potential clients (maybe that's <span>you</span>?), as well as anyone else who might be interested in the things I make.</h1>
+                    <h1 className="info">One thing I love about this website is that it's my own. That means that I can add a bunch of <span>Easter eggs</span> around it. Seriously, it's actually kinda fun making them.</h1>
                     <h1 className="info">Also I felt left out that everyone else has a website and I didn't lol.</h1>
 
                 </div>

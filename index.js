f46acedadd47007b70c87a8b5d4a41170a6b8c6d
APixel Visuals <apixelvisualsofficial@gmail.com>
2019-04-17 21:06:23 -0700

    Added support for SSL

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 (async () => {
     //Modules
     require("dotenv").config();
-    const { createServer } = require("http");
+    const fs = require("fs");
+    const { createServer } = require("https");
     const parseURL = require("url").parse;
     const next = require("next");
 
@@ -13,12 +14,17 @@
     await app.prepare();
 
     //Create server
-    createServer((req, res) => {
+    createServer(
+        process.env.DEV === "true" ?
+            { key: fs.readFileSync("/home/apixel/key.pem"), cert: fs.readFileSync("/home/apixel/server.crt") } :
+            { key: fs.readFileSync("/etc/letsencrypt/live/apixel.me/privkey.pem"), cert: fs.readFileSync("/etc/letsencrypt/live/apixel.me/fullchain.pem") },
+        (req, res) => {
 
-        //Parse URL
-        const url = parseURL(req.url, true);
+            //Parse URL
+            const url = parseURL(req.url, true);
 
-        //Handle reqest
-        handler(req, res, url);
-    }).listen(3001);
+            //Handle reqest
+            handler(req, res, url);
+        }
+    ).listen(3001);
 })();

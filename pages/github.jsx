94651a4909fba2b228c926a9e65f1272ebb717d9
APixelVisuals <apixelvisualsofficial@gmail.com>
2020-06-19 03:18:39 -0700

    Added twitter and github pages

---
+++ pages/github.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from "../components/Head";
+
+export default class GitHub extends React.Component {
+
+    render = () => {
+        return (
+            <Head
+                title="GitHub - APixel Visuals"
+                description="Check out my GitHub."
+                redirect="https://github.com/APixelVisuals"
+            />
+        );
+    };
+
+};
